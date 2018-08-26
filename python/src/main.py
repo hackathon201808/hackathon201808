db46ab84c4e870ee346eb5d7e2b2f0b67332ce71
Shundo Kishi <syundo0730@gmail.com>
2018-08-26 11:35:00 +0900

    use DMP calculated yaw-pitch-roll angles

--- python/src/main.py
+++ python/src/main.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 
 PORT = '/dev/ttyUSB0'
-BAUDRATE = 38400
+BAUDRATE = 115200
 
 data_frame = pd.DataFrame(columns=['time', 'ax', 'ay', 'az', 'gx', 'gy', 'gz'])
 start_at = time.time()
@@ -18,9 +18,6 @@ queues = {
     'ax': collections.deque(maxlen=FILTER_SIZE),
     'ay': collections.deque(maxlen=FILTER_SIZE),
     'az': collections.deque(maxlen=FILTER_SIZE),
-    'gx': collections.deque(maxlen=FILTER_SIZE),
-    'gy': collections.deque(maxlen=FILTER_SIZE),
-    'gz': collections.deque(maxlen=FILTER_SIZE),
 }
 SHAKED_THRESHOLD = 25000
 
@@ -33,33 +30,39 @@ def main():
             data = raw_str.split('\\t')
             if len(data) < 7:
                 continue
+            ax, ay, az, y, p, r = data[1:]
+            ax = int(ax)
+            ay = int(ax)
+            az = int(az)
+            y = float(y)
+            p = float(p)
+            r = float(r)
             if not initialized:
                 initialized = True
                 start_at = time.time()
-            sensor_data = data[1:]
-            should_save = on_data_recieved(*sensor_data)
+            should_save = on_data_recieved(ax, ay, az, y, p, r)
             if should_save:
                 save()
                 break
 
-def on_data_recieved(ax, ay, az, gx, gy, gz):
+def on_data_recieved(ax, ay, az, y, p, r):
     # print(ax, ay, az, gx, gy, gz)
-    record(ax, ay, az, gx, gy, gz)
-    acc, acc_array = get_filtered_values()
+    record(ax, ay, az)
+    acc, acc_array= get_filtered_values()
+    ypr_array = np.array([y, p, r], dtype=float)
     ax_norm = np.linalg.norm(acc_array)
-    if ax_norm > SHAKED_THRESHOLD and acc['ay'] > 0:
+    if ax_norm > SHAKED_THRESHOLD and acc['ay'] < 0:
+        print(ax_norm)
         print(time.time(), 'shaked!!!!!', acc['ay'])
-    # return make_log(ax, ay, az, gx, gy, gz)
+    roll = ypr_array[1]
+    print(roll)
     return False
 
-def record(ax, ay, az, gx, gy, gz):
+def record(ax, ay, az):
     global queues
     queues['ax'].append(ax)
     queues['ay'].append(ay)
     queues['az'].append(az)
-    queues['gx'].append(gx)
-    queues['gy'].append(gy)
-    queues['gz'].append(gz)
 
 def get_filtered_values():
     filtered = {}
