9fb6996c2e7519e16b242edeb685d15037b55f20
Shundo Kishi <syundo0730@gmail.com>
2018-08-26 13:13:30 +0900

    fix bags

--- python/src/gesture_detector.py
+++ python/src/gesture_detector.py
@@ -5,8 +5,9 @@ import numpy as np
 
 class GestureDetector:
     RECORD_SIZE = 50
-    RAISE_GAP = 1000
-    RAISE_BETWEEN = 2.0 # 2回振ったと判定される間隔
+    RAISE_GAP = 2000
+    BLIND_TIME = 0.5 # 連続してraiseを検知しない時間
+    RAISE_BETWEEN = 1.5 # 2回振ったと判定される間隔
     def __init__(self):
         self.queues = {
             'ax': collections.deque(maxlen=self.RECORD_SIZE),
@@ -21,18 +22,19 @@ class GestureDetector:
         self.queues['az'].append(az)
 
     def _elapsed_from_last_raise(self):
-        return self.last_raise_occured_at - time.time()
+        return time.time() - self.last_raise_occured_at
     
     def _is_raised(self, ax, ay, az):
         log_az = self.queues['az']
-        avg_az = np.average(log_az)
+        record_weight = np.array([0.96**i for i in range(len(log_az))])
+        avg_az = np.average(log_az, weights=record_weight)
         gap = az - avg_az
 
         if gap < self.RAISE_GAP:
             return False
 
-        # 一度検出したら0.5秒は検知しない
-        if self._elapsed_from_last_raise() < 0.5:
+        # 一度検出したらしばらくは検知しない
+        if self._elapsed_from_last_raise() < self.BLIND_TIME:
             return False
 
         self.last_raise_occured_at = time.time()
