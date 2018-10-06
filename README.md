630f22274a520e6258b4b8c9d8f84333028f12de
Shundo Kishi <shundo.kishi@groove-x.com>
2018-10-06 17:12:27 +0900

    update README and comments

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
-# motion estimator
+ASCII Hackathon 2018 DooTu
+---
+This resources are made for ASCII IOT dev hackathon.
 
-* arduino/imu ... arduinoでMPU6050を読み取ってシリアル通信で送ります
-* python/src ... IMUが振られたのを判定するやつ
-* python/analytics ... IMUのデータをしっかり分析したかったけど時間がない
+# Requirements
+* pipenv (recommended)
+* pyenv (recommended)
+
+# Install
+Use command bellow (If you use pipenv)
+
+```
+$ cd python
+$ pipenv install
+```
+
+If you want to install resources for development, also use command bellow.
+It will install jupyter notebook and so on, which is used to analize IMU data.
+```
+$ pipenv install --dev
+```
+
+# Usage
+## Send IMU data from M5Stack Gray by Bluetooth
+Open `arduino/m5stackgray/imu_bt/imu_bt.ino` by Arduino IDE and Upload it to M5Stack Gray.
+
+## Detect someone go through the gate by M5Stack
+Open `arduino/m5stackgray/gate/gate.ino` by Arduino IDE and Upload it to M5Stack Gray.
+
+## Receive IMU data from M5Stack and filter by python
+Run command bellow
+```
+pipenv run start_bt_receiver
+```
+or just run script directly by python. (version python(3 >) is required)
+```
+python python/src/main.py
+```
+
+## Display game state
+Open `unity/display_practice/Assets/Main.unity` by Unity and run it.
--- python/src/imu_filter.py
+++ python/src/imu_filter.py
@@ -18,6 +18,10 @@ class IMUFilter:
         self._on_filtered = on_filtered
 
     def record(self, data):
+        """
+        IMUから取得した情報を記録する
+        data: M5Bridgeから渡されるIMUの情報
+        """
         for k in ['ax', 'ay', 'az']:
             self.queues[k].append(data[k])
         acc = self.get_natural_accel()
@@ -36,22 +40,35 @@ class IMUFilter:
                 self._on_filtered(filtered)
 
     def get_natural_accel(self):
+        """
+        重力要素を除去した加速度成分を取得する
+        TODO: ローパスフィルタを用いて重力要素を除去しているだけなので、遅い動きを捉えられない
+        TODO: 姿勢情報を使って除去する
+        """
         non_biased = {}
         for k in ['ax', 'ay', 'az']:
             v = self.queues[k]
             if len(v) < 3:
                 continue
             a = np.array(v, dtype=float)
+            # 最新より3つ前以前のデータの平均
             ave = np.average(a[2:])
+            # 最新2件の平均
             current_ave = np.average(a[:2])
             non_biased[k] = current_ave - ave
         return non_biased
 
     def get_pseudo_velocity(self):
+        """
+        一定期間で積分した擬似的な速度を取得する
+        """
         keys = ['vx', 'vy', 'vz']
         vel = {}
+        # 重力要素を除去した加速度情報を用いて速度を求める
         for k, nk in zip(keys, ['nx', 'ny', 'nz']):
             v = self.queues[nk]
             a = np.array(v, dtype=float)
+            # TODO: 数値積分における微小時間を適当に0.1にせずにちゃんと計測するか調整する
+            # TODO: より精度の高い数値積分法を用いる
             vel[k] = np.sum(a) * 0.1
         return vel
